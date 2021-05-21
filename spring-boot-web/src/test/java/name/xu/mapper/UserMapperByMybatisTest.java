49aae6deaf2b6b65ef0050327be9d5c46f322b0f
Xu <2542610526@qq.com>
2021-05-21 22:45:51 +0800

    使用 Mybatis 的单元测试

--- spring-boot-web/src/test/java/name/xu/mapper/UserMapperByMybatisTest.java
+++ spring-boot-web/src/test/java/name/xu/mapper/UserMapperByMybatisTest.java
@@ -1,26 +1,29 @@
 package name.xu.mapper;
 
 import lombok.extern.slf4j.Slf4j;
-import name.xu.Application;
 import name.xu.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 /**
  * mapper 单元测试
- * 使用 SpringBootTest
+ * 使用 @MybatisTest
  *
  * @author Created by HuoXu
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = Application.class)
+@RunWith(SpringRunner.class)
+// 这块需要引入依赖 mybatis的测试依赖jar
+@MybatisTest
+// 这个注解的意义是指定了默认数据源
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Slf4j
-public class UserMapperTest {
+public class UserMapperByMybatisTest {
     @Resource
     private UserMapper userMapper;
 
